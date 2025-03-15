fddff322f761483137e5fc79a871c56c71bd86b7
Dhuvesh <dhuvesh.salian@gmail.com>
2025-03-15 11:36:38 +0530

    validation complete

--- src/app/auth/components/auth/register/register.component.ts
+++ src/app/auth/components/auth/register/register.component.ts
@@ -9,16 +9,40 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-    registerForm: FormGroup;
-    constructor(private formBuilder: FormBuilder) {
-        this.registerForm = this.formBuilder.group({
-            name: ['', [Validators.required , Validators.minLength(2)]],
-            email: ['', [Validators.required, Validators.email]],
-            password: ['', [Validators.required, Validators.minLength(6)]],
-            confirmpassword: ['', [Validators.required, Validators.minLength(6)]]
-        });
+  registerForm: FormGroup;
+  constructor(private formBuilder : FormBuilder){
+    this.registerForm = this.formBuilder.group({
+      name : ['',[Validators.required,Validators.minLength(4)]],
+      email : ['',[Validators.required,Validators.email]],
+      password : ['',[Validators.required,Validators.minLength(5)]],
+      confirmPassword : ['',[Validators.required,Validators.minLength(5)]],
+    });
+  }
+  registerSubmit(){
+    // console.log("Submitted form");
+    if(this.registerForm.valid){
+      console.log('Success' + JSON.stringify(this.registerForm.value))
+      alert("Form submitted successfully")
+    }else{
+      console.log(this.registerForm.errors);
+      this.printErrors();
     }
-    registerSubmit() {
-        console.log('form submitted');
+  }
+  printErrors() {
+    const controls = this.registerForm.controls;
+
+    for (const controllerName in controls) {
+      const control = controls[controllerName];
+      if (control.invalid && control.touched) {
+        const errors = control.errors;
+        if (errors) {
+          console.log(`${controllerName} has the following errors:`);
+          for (const error in errors) {
+            console.log(` ${error}: ${JSON.stringify(errors[error])}}`);
+          }
+        }
+      }
     }
+
+  }
 }
